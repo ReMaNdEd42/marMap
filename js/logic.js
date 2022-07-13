425d85166568998e0d6ab7015f0f820c3fc01ec8
ReMaNdEd42 <yaliev.02@mail.ru>
2022-07-13 12:18:55 +0300

    added a scale function

--- js/logic.js
+++ js/logic.js
@@ -7,6 +7,7 @@ const keyMap = {
 };
 
 const scroolInertia = {
+  isTouchEnd: true,
   speedX: 0,
   speedY: 0,
   deceleration: 3
@@ -22,30 +23,39 @@ document.addEventListener("keydown", (e) => checkKey(e.code, true));
 document.addEventListener("keyup", (e) => checkKey(e.code, false));
 
 canvas.addEventListener("touchstart", function (e) {
-  touches[0].startX = e.changedTouches[0].screenX;
-  touches[0].startY = e.changedTouches[0].screenY;
+  console.log(e);
+  touches[0].startX = e.touches[0].screenX;
+  touches[0].startY = e.touches[0].screenY;
+
   touches[0].start = e.timeStamp;
 
-  touches[0].endX = e.changedTouches[0].screenX;
-  touches[0].endY = e.changedTouches[0].screenY;
+  touches[0].endX = e.touches[0].screenX;
+  touches[0].endY = e.touches[0].screenY;
+  
+  scroolInertia.isTouchEnd = false;
+  if(e.touches>1){
+    touches[1].startX = e.touches[1].screenX;
+    touches[1].startY = e.touches[1].screenY;
+    
+    touches[1].start = e.timeStamp;
+    
+    touches[1].endX = e.touches[1].screenX;
+    touches[1].endY = e.touches[1].screenY;
+  }
+  
 });
 
 canvas.addEventListener("touchmove", function (e) {
-  console.log(e);
-  touches[0].endX = e.changedTouches[0].screenX;
-  touches[0].endY = e.changedTouches[0].screenY;
-  touches[0].stop = e.timeStamp;
+  if (e.touches.length == 1) scrool(e);
+  if (e.touches.length == 2) scale(e);
 
-  scroolInertia.speedX = (touches[0].endX - touches[0].startX) / (touches[0].stop - touches[0].start) * 30;
-  scroolInertia.speedY = (touches[0].endY - touches[0].startY) / (touches[0].stop - touches[0].start) * 30;
-
-  if (e.changedTouches.length == 1) scrool(e);
 });
 
 canvas.addEventListener("touchend", function (e) {
+  scroolInertia.isTouchEnd = true;
 });
 
-const mMap = { sprite: new Image(), x: 0, y: 0 }
+const mMap = { sprite: new Image(), x: 0, y: 0, scale: 1 }
 
 mMap.sprite.src = "images/map.jpg"
 
@@ -56,18 +66,42 @@ mMap.sprite.onload = function () {
 
 function scrool(e) {
   touches[0].stop = e.timeStamp;
-  touches[0].endX = e.changedTouches[0].screenX;
-  touches[0].endY = e.changedTouches[0].screenY;
+  touches[0].endX = e.touches[0].screenX;
+  touches[0].endY = e.touches[0].screenY;
+
+  scroolInertia.speedX = (touches[0].endX - touches[0].startX) / (touches[0].stop - touches[0].start) * 50;
+  scroolInertia.speedY = (touches[0].endY - touches[0].startY) / (touches[0].stop - touches[0].start) * 50;
 
   mMap.x += (touches[0].endX - touches[0].startX);
   mMap.y += (touches[0].endY - touches[0].startY);
 
-  touches[0].startX = e.changedTouches[0].screenX;
-  touches[0].startY = e.changedTouches[0].screenY;
+  touches[0].startX = e.touches[0].screenX;
+  touches[0].startY = e.touches[0].screenY;
   touches[0].start = e.timeStamp;
 }
 
+function scale(e) {
+  touches[0].stop = e.timeStamp;
+  touches[0].endX = e.touches[0].screenX;
+  touches[0].endY = e.touches[0].screenY;
 
+  touches[1].stop = e.timeStamp;
+  touches[1].endX = e.touches[1].screenX;
+  touches[1].endY = e.touches[1].screenY;
+
+  let s = Math.sqrt((touches[0].endX - touches[1].endX) ** 2 + (touches[0].endY - touches[1].endY) ** 2) /
+  Math.sqrt((touches[0].startX - touches[1].startX) ** 2 + (touches[0].startY - touches[1].startY) ** 2);
+  console.log(s);
+  mMap.scale *= s
+
+  touches[0].startX = e.touches[0].screenX;
+  touches[0].startY = e.touches[0].screenY;
+  touches[0].start = e.timeStamp;
+
+  touches[1].startX = e.touches[1].screenX;
+  touches[1].startY = e.touches[1].screenY;
+  touches[1].start = e.timeStamp;
+};
 
 function animation() {
   update();
@@ -76,13 +110,13 @@ function animation() {
 }
 
 function update() {
-  
-  if (keyMap.KeyA == true) mMap.x += 5;
-  if (keyMap.KeyD == true) mMap.x -= 5;
-  if (keyMap.KeyW == true) mMap.y += 5;
-  if (keyMap.KeyS == true) mMap.y -= 5;
 
-  if (Math.abs(scroolInertia.speedX) > scroolInertia.deceleration) {
+  if (keyMap.KeyA == true) mMap.x -= 5;
+  if (keyMap.KeyD == true) mMap.x += 5;
+  if (keyMap.KeyW == true) mMap.y -= 5;
+  if (keyMap.KeyS == true) mMap.y += 5;
+
+  if (Math.abs(scroolInertia.speedX) > scroolInertia.deceleration && scroolInertia.isTouchEnd) {
     mMap.x += scroolInertia.speedX;
     scroolInertia.speedX > 0 ?
       scroolInertia.speedX -= scroolInertia.deceleration :
@@ -90,7 +124,7 @@ function update() {
   }
   else scroolInertia.speedX = 0;
 
-  if (Math.abs(scroolInertia.speedY) > scroolInertia.deceleration) {
+  if (Math.abs(scroolInertia.speedY) > scroolInertia.deceleration && scroolInertia.isTouchEnd) {
     mMap.y += scroolInertia.speedY;
     scroolInertia.speedY > 0 ?
       scroolInertia.speedY -= scroolInertia.deceleration :
@@ -104,13 +138,7 @@ function update() {
 
 function render() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(mMap.sprite, mMap.x, mMap.y);
-  try {
-    // console.log(tuchPosition[0].startX);
-  }
-  catch {
-
-  }
+  context.drawImage(mMap.sprite, mMap.x, mMap.y, mMap.sprite.width * mMap.scale, mMap.sprite.height * mMap.scale);
 }
 
 function checkKey(code, flag) {
@@ -120,11 +148,11 @@ function checkKey(code, flag) {
       keyMap.KeyA = flag;
       break;
 
-    case "KeyW": //Вправо
+    case "KeyW": //Вверх
       keyMap.KeyW = flag;
       break;
 
-    case "KeyD": //Вверх
+    case "KeyD": //Вправо
       keyMap.KeyD = flag;
       break;
 
